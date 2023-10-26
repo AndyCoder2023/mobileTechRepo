d804aae3e57d1b47404b6d7bd7e3a47709e86239
Andrew <a.webster2@rgu.ac.uk>
2023-10-26 15:10:40 +0100

    changes and fixes

--- app/src/main/java/com/example/mobiletech/MainActivity.java
+++ app/src/main/java/com/example/mobiletech/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.mobiletech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
