8267a348a5085671eb86c3b71a23d552b88e14e5
Andy <a.webster2@rgu.ac.uk>
2023-11-03 13:24:44 +0000

    topic 4 task 4 needs correction with java code buttons not being recognised.

--- app/src/main/java/com/example/mobiletech/LocationSelectionFragment.java
+++ app/src/main/java/com/example/mobiletech/LocationSelectionFragment.java
@@ -2,18 +2,23 @@ package com.example.mobiletech;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link LocationSelectionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LocationSelectionFragment extends Fragment {
+public class LocationSelectionFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,5 +65,36 @@ public class LocationSelectionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_select_location, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //    for navigating to the LocationSelectionFragment
+        Button getForecastButton = getView().findViewById(R.id.btnGetForecast);
+        getForecastButton.setOnClickListener(this);
+
+        //    for navigating to the ForecastFragment
+        Button gpsForecastButton = getView().findViewById(R.id.);
+        gpsForecastButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+//        Get the nav controller
+        NavController navController = Navigation.findNavController(view);
+        switch (view.getId()){
+
+//            TODO: Get the number of days to get the forecast for
+            case R.id.btnGetForecast:
+//            Navigate to the LocationConfirmationFragment
+                navController.navigate(R.id.action_selectLocationFragment_to_locationConfirmationFragment);
+                break;
+            case R.id.btnGpsForecast:
+//                TODO: Get the location from GPS
+//                Navigate to the Forecast Fragment
+                ((NavController) navController).navigate(R.id.action_selectLocationFragment_to_forecastFragment);
+        }
     }
 }
