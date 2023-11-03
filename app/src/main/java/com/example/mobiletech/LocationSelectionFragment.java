dd7545694aa25967fa620f0a2a08df7ba6aed52d
Andy <a.webster2@rgu.ac.uk>
2023-11-03 11:29:06 +0000

    topic 4 task 3 sorted and done. added navigation between fragments and set up home fragment as select location fragment.

--- app/src/main/java/com/example/mobiletech/LocationSelectionFragment.java
+++ app/src/main/java/com/example/mobiletech/LocationSelectionFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SelectLocationFragment#newInstance} factory method to
+ * Use the {@link LocationSelectionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SelectLocationFragment extends Fragment {
+public class LocationSelectionFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class SelectLocationFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public SelectLocationFragment() {
+    public LocationSelectionFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class SelectLocationFragment extends Fragment {
      * @return A new instance of fragment SelectLocationFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SelectLocationFragment newInstance(String param1, String param2) {
-        SelectLocationFragment fragment = new SelectLocationFragment();
+    public static LocationSelectionFragment newInstance(String param1, String param2) {
+        LocationSelectionFragment fragment = new LocationSelectionFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
