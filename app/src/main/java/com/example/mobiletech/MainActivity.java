8e797960da7b2e2f6997e73361f7c8fc9f122ab3
Andrew <a.webster2@rgu.ac.uk>
2023-11-09 15:32:48 +0000

    fixed the widget sizing.

--- app/src/main/java/com/example/mobiletech/MainActivity.java
+++ app/src/main/java/com/example/mobiletech/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-//    todo: fix this code, video point at 13:40
+    //    todo: fix this code, video point at 13:40
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 //        handle settings item selection
@@ -47,4 +47,6 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
             }
         }
+        return false;
+    }
 }
